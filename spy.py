a3b089a09767dedac9dbee6b04aa1201c883c1f8
Ritul <76278629+RitulSherkar@users.noreply.github.com>
2021-06-04 18:50:53 +0530

    Add files via upload

---
+++ spy.py
@@ -0,0 +1,26 @@
+import discord, chalk
+from discord.ext import commands
+channel_which_you_want_to_spy_on = #put the channel id here DO NOT USE INVERTED COMMA'S 
+channel_which_the_messages_will_be_sent_on = #put the channel id here DO NOT USE INVERTED COMMA'S 
+
+
+
+bot = commands.Bot(command_prefix = "|")
+@bot.event
+async def on_ready():
+	print('The bot is online')
+
+
+
+@bot.event
+async def on_message(message):
+	if message.channel.id == channel_which_you_want_to_spy_on :
+		cmd = message.content
+		channel = bot.get_channel(channel_which_the_messages_will_be_sent_on)
+		print(cmd)
+		msg = message.author
+		await channel.send(f'{message.author} : {cmd}')
+
+
+
+bot.run('ODQ5Mjg0NDUxNzUzNTI1MzI4.YLY7yA.207I_1FteBEnFfc1lm9HB9eTdxE')
